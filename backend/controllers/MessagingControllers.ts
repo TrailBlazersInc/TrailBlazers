54f50420060fd558e4ac2796bf39b35efe6ff816
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2025-03-01 15:04:49 -0800

    some backend update on messages

--- backend/controllers/MessagingControllers.ts
+++ backend/controllers/MessagingControllers.ts
@@ -5,11 +5,39 @@ import {Chat} from "../models/chat";
 
 export class MessagingControllers {
     async getChats(req: Request, res: Response, next: NextFunction){
-        
-        chats = Chat.find()
-        res.status(200).send('hello')
+        const userId: String = req.body.user
+        try{
+            const chats = await Chat.find({ members: userId });
+            if (chats){
+                res.status(200).json(chats)
+            } 
+        } catch(error){
+            res.status(400).send("No chats found")
+        }
+    }
+
+    async getMessagesAfter(req: Request, res: Response, next: NextFunction){
+      
+
+    }
+    async postChat(req: Request, res: Response, next: NextFunction){
+        try{
+            const users = req.body.userList
+            const chatName = req.body.chatName
+            const chat = new Chat(chatName,users, {})
+            await chat.save()
+            res.status(200).json(chat)
+        } catch(error){
+            res.status(500).send("Error creating Chat")
+        }
     }
     async postMessage(req: Request, res: Response, next: NextFunction){
+
         let myChat = await Chat.findOne(req.body.chatId);
     }
+
+    async addUser(req: Request, res: Response, next: NextFunction){
+        let myChat = await Chat.findOne(req.body.chatId);
+    }
+
 }
--- backend/routes/MessagingRoutes.ts
+++ backend/routes/MessagingRoutes.ts
@@ -1,4 +1,8 @@
 import { MessagingControllers } from "../controllers/MessagingControllers"
+import {body, param} from "express-validator"
+import mongoose from "mongoose"
+import {User} from "../models/user"
+import {Chat} from "../models/chat"
 
 const controllers = new MessagingControllers()
 
@@ -7,6 +11,61 @@ export const MessagingRoutes = [
         method: "get",
         route: "/chats",
         action: controllers.getChats,
-        validation: []
+        validation: [
+            body("userId").isMongoId()
+        ]
+    },
+    {
+        method: "get",
+        route: "/messagesAfter",
+        action: controllers.getMessagesAfter,
+        validation: [
+            body("messageId").isMongoId()
+        ]
+
+    },
+    {
+        method: "post",
+        route: "chat",
+        action: controllers.postChat,
+        validation:[
+            body("userList")
+                .isArray().withMessage("user List must be an Array of IDs")
+                .notEmpty().withMessage("userList must not be empty")
+                .custom(async (ids) =>{
+                    if (!ids.every( (id: any) => mongoose.Types.ObjectId.isValid(id))) {
+                        throw new Error("One or more IDs are invalid");
+                    }
+    
+                    // Check that IDs exist in the database
+                    const existingUsers = await User.find({ _id: { $in: ids } });
+                    if (existingUsers.length !== ids.length) {
+                        throw new Error("One or more users do not exist");
+                    }
+
+                }),
+            body("chatName")
+                .isString()
+                .isLength({ max: 50 }).withMessage("Chatname Must be Shorter at Max 50 Characters")
+        ]
+    },
+    {
+        method: "post",
+        route: "Message",
+        action: controllers.postMessage,
+        validation:[
+            body("userId").isMongoId(),
+            body("chatId").isMongoId(),
+            body("content").isString()
+        ]
+    },
+    {
+        method: "put",
+        route: "addUser",
+        action: controllers.addUser,
+        validation: [
+            body("userId").isMongoId(),
+            body("chatId").isMongoId()
+        ]
     }
 ]
