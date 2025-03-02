d6b02cea18582fcebd804f16f64542caa2b9a743
qianyi-yu <yuqianyi1409@gmail.com>
2025-03-01 22:40:01 -0800

    Add recommendation button to Home page

--- android_app/app/src/main/java/com/example/cpen321project/HomeActivity.kt
+++ android_app/app/src/main/java/com/example/cpen321project/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.cpen321project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +27,12 @@ class HomeActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.manageProfileButton).setOnClickListener() {
             val intent = Intent(this, ManageProfile::class.java)
+            intent.putExtra("tkn", tkn)
+            intent.putExtra("email", email)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.recommendationButton).setOnClickListener {
+            val intent = Intent(this, Recommendation::class.java)
             intent.putExtra("tkn", tkn)
             intent.putExtra("email", email)
             startActivity(intent)
