a20080d018e42aadfd14275707ef8a97c77f210c
AmanvirSamra <amanvirsamra@hotmail.com>
2025-03-01 21:13:05 -0800

    Home Page Fix

--- android_app/app/src/main/java/com/example/cpen321project/HomeActivity.kt
+++ android_app/app/src/main/java/com/example/cpen321project/HomeActivity.kt
@@ -13,6 +13,10 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        val tkn = extras?.getString("tkn") ?: ""
+        val email = extras?.getString("email") ?: ""
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,6 +24,8 @@ class HomeActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.manageProfileButton).setOnClickListener() {
             val intent = Intent(this, ManageProfile::class.java)
+            intent.putExtra("tkn", tkn)
+            intent.putExtra("email", googleIdTokenCredential.id)
             startActivity(intent)
         }
         findViewById<Button>(R.id.signOutButton).setOnClickListener() {
