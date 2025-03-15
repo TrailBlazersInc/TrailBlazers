2957ccbbd61772ef47cffdf7196acb4404a650d2
AmanvirSamra <amanvirsamra@hotmail.com>
2025-03-14 17:39:10 -0700

    Updated formatting for User Tests

---
+++ backend/tests/mocks/userControllers.mock.test.ts
@@ -0,0 +1,90 @@
+jest.mock('google-auth-library');
+
+import { describe, expect, test, beforeAll, afterAll, jest } from '@jest/globals';
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { server } from '../../index';
+import { User } from '../../models/user';
+import crypto from 'crypto';
+
+const validEmail = crypto.randomUUID() + '@example.com';
+const invalidEmail = "invalidmockUser@example.com";
+const distance = '5km';
+const time = '2 hour';
+const pace = 5;
+const availability = {
+    monday: true,
+    tuesday: false,
+    wednesday: true,
+    thursday: false,
+    friday: true,
+    saturday: false,
+    sunday: true,
+  };
+
+afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+});
+
+describe('PUT /User/:email (mock)', () => {
+    // Mocked behavior: User.updateOne throws an error
+    // Input: validEmail with updated preferences
+    // Expected status code: 500
+    // Expected behavior: Failure to connect to database, returns error message
+    // Expected output: Error 'Error Updating User'
+    test('cannot find user to update User data, should return 500 and error message', async () => {
+        jest.spyOn(User, 'updateOne').mockRejectedValue(new Error('Internal Server Error'));
+
+        const response = await request(server).put('/User/' + invalidEmail).send({ distance : distance, time : time, pace : pace, availability : availability });
+        expect(response.status).toBe(500);
+        expect(response.body.error).toBe('Error Updating User');
+    });
+});
+
+describe('POST /api/v1/auth/google (mock)', () => {
+    test('Invalid GoogleId', async () => {
+        //Input: Invalid GoogleID and false flag for admin
+        //Expected Status Code: 500
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'Internal Server Error'
+        const response = await request(server).post('/api/v1/auth/google').send({ googleId : 'invalid_mock_token', admin : false });
+        expect(response.status).toBe(500);
+        expect(response.body.error).toBe('Internal Server Error');
+    });
+    test('Valid GoogleID but no payload', async () => {
+        //Input: Valid GoogleID and false flag for admin
+        //Expected Status Code: 400
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'Bad Request'
+        const response = await request(server).post('/api/v1/auth/google').send({ googleId : 'empty_payload', admin : false });
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('Bad Request');
+    });
+    test('Valid Standard User Request', async () => {
+        //Input: Valid GoogleID and false flag for admin
+        //Expected Status Code: 200
+        //Expected Behavior: Sends back sessionID and user info
+        //Expected output: Token for SessionID, user object, flag for banned, false flag for admin
+        const response = await request(server).post('/api/v1/auth/google').send({ googleId : 'valid_mock_token', admin : false });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('status', 'success');
+        expect(response.body).toHaveProperty('token');
+        expect(response.body).toHaveProperty('new_user');
+        expect(response.body).toHaveProperty('banned');
+        expect(response.body).toHaveProperty('admin', false);
+    });
+    test('Valid Admin Request', async () => {
+        //Input: Valid GoogleID and true flag for admin
+        //Expected Status Code: 200
+        //Expected Behavior: Sends back sessionID and user info
+        //Expected output: Token for SessionID, user object, flag for banned, true flag for admin
+        const response = await request(server).post('/api/v1/auth/google').send({ googleId : 'no_name', admin : true });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('status', 'success');
+        expect(response.body).toHaveProperty('token');
+        expect(response.body).toHaveProperty('new_user');
+        expect(response.body).toHaveProperty('banned');
+        expect(response.body).toHaveProperty('admin', true);
+    });
+});
---
+++ backend/tests/no-mocks/userController.test.ts
@@ -0,0 +1,99 @@
+jest.mock('google-auth-library');
+
+import { describe, expect, test, beforeAll, afterAll, jest } from '@jest/globals';
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { server } from '../../index';
+import { User } from '../../models/user';
+import crypto from 'crypto';
+
+const validEmail = crypto.randomUUID() + '@example.com';
+const invalidEmail = "invalidmockUser@example.com";
+const distance = '5km';
+const time = '2 hour';
+const pace = 5;
+const availability = {
+    monday: true,
+    tuesday: false,
+    wednesday: true,
+    thursday: false,
+    friday: true,
+    saturday: false,
+    sunday: true,
+  };
+
+beforeAll(async () => {
+    await User.create({
+        social_id: 'mocked_user_id',
+        email: validEmail,
+        first_name: 'Mock', 
+        last_name: 'User', 
+        pace: 6, 
+        distance: '10km',
+        time: '1 hour',
+        availability: {
+          monday: false,
+          tuesday: false,
+          wednesday: false,
+          thursday: false,
+          friday: false,
+          saturday: false,
+          sunday: false,
+        },
+        longitude: '123.1234', 
+        latitude: '12.1234', 
+        banned: false, 
+        admin: false, 
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+});
+
+describe('GET /user/:email', () => {
+    test('Valid request', async () => {
+        //Input: Valid Email
+        //Expected Status Code: 200
+        //Expected Behavior: Returns User Data
+        //Expected output: User object
+        const response = await request(server).get('/User/' + validEmail).send({});
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('status', 'success');
+        expect(response.body).toHaveProperty('user');
+    });
+});
+
+describe('PUT /user/:email', () => {
+    test('Valid request', async () => {
+        //Input: Valid Email and updated preferences
+        //Expected Status Code: 200
+        //Expected Behavior: Updates the User DB of the user with updated preferences and returns success message
+        //Expected output: Message 'User data updated successfully'
+        const response = await request(server).put('/User/' + validEmail).send({ distance : distance, time : time, pace : pace, availability : availability });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message', 'User data updated successfully');
+    });
+    test('Invalid user email', async () => {
+        //Input: Invalid Email
+        //Expected Status Code: 400
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'User not found'
+        const response = await request(server).put('/User/' + invalidEmail).send({ distance : distance, time : time, pace : pace, availability : availability });
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('User not found');
+    });
+});
+
+describe('POST /api/v1/auth/google', () => {
+    test('No GoogleID provided', async () => {
+        //Input: nothing
+        //Expected Status Code: 400
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'ID is required'
+        const response = await request(server).post('/api/v1/auth/google').send({});
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('ID is required');
+    });
+});
