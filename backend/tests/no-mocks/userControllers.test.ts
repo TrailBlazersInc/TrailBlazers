68d8cb9603dd93ce9adc8bf49814e5dc0be5ef59
AmanvirSamra <90374550+AmanvirSamra@users.noreply.github.com>
2025-03-27 19:22:35 -0700

    Added No Mock tests for Ban and Reporting Users

--- backend/tests/no-mocks/userControllers.test.ts
+++ backend/tests/no-mocks/userControllers.test.ts
@@ -10,6 +10,8 @@ const invalidEmail = "invalidmockUser@example.com";
 const distance = '5km';
 const time = '2 hour';
 const pace = 5;
+const aggressor_email = invalidEmail;
+const description = "Harrassment";
 const availability = {
     monday: true,
     tuesday: false,
@@ -96,5 +98,61 @@ describe('POST /api/v1/auth/google', () => {
         const response = await request(server).post('/api/v1/auth/google').send({});
         expect(response.status).toBe(400);
         expect(response.body.error).toBe('ID is required');
+    });
+});
+
+describe('PUT /ban/:email', () => {
+    test('Invalid email provided', async () => {
+        //Input: Invalid Email
+        //Expected Status Code: 404
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'User not found'
+        const response = await request(server).put('/ban/' + invalidEmail).send({});
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe('User not found');
+    });
+    test('Valid request', async () => {
+        //Input: Valid Email
+        //Expected Status Code: 200
+        //Expected Behavior: Updates the user associated with the email in the User DB to reflect banned status
+        //Expected output: Message 'User banned'
+        const response = await request(server).put('/ban/' + validEmail).send({});
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message', 'User banned');
+    });
+});
+
+describe('GET /report', () => {
+    test('Valid Request', async () => {
+        //Input: nothing
+        //Expected Status Code: 200
+        //Expected Behavior: Returns a object that contains all the reports in the report DB
+        //Expected output: formattedReports object
+        const response = await request(server).get('/report').send({});
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+});
+
+describe('POST /report/:email', () => {
+    test('Valid Request', async () => {
+        //Input: Email of user who sent report, aggressor_email, description
+        //Expected Status Code: 200
+        //Expected Behavior: Adds the report into the report DB and returns the added report and sends a success status
+        //Expected output: report object and success status
+        const response = await request(server).post('/report/' + validEmail).send({ aggressor_email, description });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('status', 'success');
+        expect(response.body).toHaveProperty('report');
+    });
+
+    test('Invalid email provided', async () => {
+        //Input: Invalid email provided
+        //Expected Status Code: 404
+        //Expected Behavior: Returns Error Message
+        //Expected output: Error 'User not found'
+        const response = await request(server).post('/report/' + invalidEmail).send({ aggressor_email, description });
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('User not found');
     });
 });
