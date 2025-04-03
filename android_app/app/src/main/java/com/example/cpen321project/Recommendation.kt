8d9881849c968d5dfe709866580bc3d676792120
qianyi-yu <yuqianyi1409@gmail.com>
2025-04-02 22:16:15 -0700

    Fix FE codacy error

--- android_app/app/src/main/java/com/example/cpen321project/Recommendation.kt
+++ android_app/app/src/main/java/com/example/cpen321project/Recommendation.kt
@@ -101,11 +101,7 @@ class Recommendation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recommendation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setupWindowInsets()
         rootView = findViewById(android.R.id.content)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -172,6 +168,14 @@ class Recommendation : AppCompatActivity() {
                 intent.putExtra("longitudes", longitudes)
                 startActivity(intent)
             }
+        }
+    }
+
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 
